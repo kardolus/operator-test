12369d34f894ce3dd02e4c4385b87d1d83f26cd2
Guillermo Kardolus <guillermok@spotify.com>
2021-10-14 16:52:51 -0400

    Create API types and controllers

--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 
 Walked through [this demo](https://medium.com/manikkothu/build-kubernetes-operator-using-kubebuilder-4bfef299757d).
 
+## Init
 ```
 go mod init github.com/kardolus/operator-test
 
 kubebuilder init --domain demo.operator-test.io
+```
 
+## Create API and controllers
+```
+kubebuilder create api --group elastic --kind MyOperatorTest --version v1
+
+Create Resource [y/n]
+y
+Create Controller [y/n]
+y
 ```
--- main.go
+++ main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	elasticv1 "github.com/kardolus/operator-test/api/v1"
+	"github.com/kardolus/operator-test/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(elasticv1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.MyOperatorTestReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "MyOperatorTest")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
